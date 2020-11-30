615d8dc2ed1b6467d0c4649629169f8490a24bf2
PutriMaylinaLilian <71164747+PutriMaylinaLilian@users.noreply.github.com>
2020-11-30 06:56:33 -0800

    ujian tengah semester

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::resource('/category', CategoryController::class);
+Route::resource('/tag', TagController::class);
+Route::resource('/post', PostController::class);
